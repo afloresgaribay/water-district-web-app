c26cb2893bdfcec8d810cf5617cd1af1c5883898
afloresgaribay <anayeli.floresgaribay@gmail.com>
2026-05-29 20:30:00 -0700

    Fix .getElementById, .textContent, and comments

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ document.querySelector("button").textContent = "Check Complete";
 }
 
 function resetMeter(){
-  document.getElementbyId("status-value").textContent = "Active";
-  document.getElementbyId("message").text-content = "";
+  document.getElementById("status-value").textContent = "Active";
+  document.getElementById("message").textContent = "";
   document.querySelector("button").disabled = false;
   document.querySelector("button").textContent = "Check Meter Status";
 }
 
 /* checkMeter() runs when the button is clicked 
-   resetMeter() resets everything back to normal /*
+   resetMeter() resets everything back to normal */
 
 /* document references the HTML page
-   getElementbyId finds HTML element by it's id
+   getElementById finds HTML element by it's id
    .textContent references the text inside the element
-   querySelector finds and returns the first HTML element/*
+   querySelector finds and returns the first HTML element */
 
 /* new Date() references JavaScript's built-in clock
  .toLocaleDateString() converts the date to a readable string */
